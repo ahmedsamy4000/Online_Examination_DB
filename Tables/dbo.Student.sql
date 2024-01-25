83ce76784d7b32aba99beb1d87a1c0b1219411da
19peter <peterashrafmail@gmail.com>
2024-01-25 21:04:59 +0200

    -Added FK Dept_Id in Student table. + Updated the relation in DB Diagram.

--- Tables/dbo.Student.sql
+++ Tables/dbo.Student.sql
@@ -1,11 +1,14 @@
 CREATE TABLE [dbo].[Student]
 (
-[ST_ID] [int] NOT NULL,
+[ST_id] [int] NOT NULL,
 [ST_Fname] [varchar] (30) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [ST_Lname] [varchar] (30) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [ST_Email] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[ST_Password] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+[ST_Password] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[Dept_Id] [int] NULL
 ) ON [PRIMARY]
 GO
-ALTER TABLE [dbo].[Student] ADD CONSTRAINT [PK_Student] PRIMARY KEY CLUSTERED ([ST_ID]) ON [PRIMARY]
+ALTER TABLE [dbo].[Student] ADD CONSTRAINT [PK_Student] PRIMARY KEY CLUSTERED ([ST_id]) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[Student] ADD CONSTRAINT [FK_Student_Department] FOREIGN KEY ([Dept_Id]) REFERENCES [dbo].[Department] ([Dep_ID])
 GO
--- Tables/dbo.Student_Course.sql
+++ Tables/dbo.Student_Course.sql
@@ -8,3 +8,5 @@ ALTER TABLE [dbo].[Student_Course] ADD CONSTRAINT [PK_Student_Course] PRIMARY KE
 GO
 ALTER TABLE [dbo].[Student_Course] ADD CONSTRAINT [FK_Student_Course_Course] FOREIGN KEY ([Crs_ID]) REFERENCES [dbo].[Course] ([Crs_ID])
 GO
+ALTER TABLE [dbo].[Student_Course] ADD CONSTRAINT [FK_Student_Course_Student] FOREIGN KEY ([St_ID]) REFERENCES [dbo].[Student] ([ST_id])
+GO
