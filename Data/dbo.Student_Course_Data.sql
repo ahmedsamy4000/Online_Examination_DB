72143a5563af41833dd885ad4504eec050633aef
ahmed samy <https://github.com/ahmedsamy4000>
2024-01-26 01:42:07 +0200

    add procedures to student and student_course

---
+++ Data/dbo.Student_Course_Data.sql
@@ -0,0 +1,2 @@
+INSERT INTO [dbo].[Student_Course] ([St_ID], [Crs_ID]) VALUES (1, 1)
+INSERT INTO [dbo].[Student_Course] ([St_ID], [Crs_ID]) VALUES (2, 2)
---
+++ Data/dbo.Student_Data.sql
@@ -0,0 +1,3 @@
+INSERT INTO [dbo].[Student] ([ST_id], [ST_Fname], [ST_Lname], [ST_Email], [ST_Password], [Dept_Id]) VALUES (1, 'usama', 'Mamdouh', 'UsamaMamdouh@gmail.com', '65546', 1)
+INSERT INTO [dbo].[Student] ([ST_id], [ST_Fname], [ST_Lname], [ST_Email], [ST_Password], [Dept_Id]) VALUES (2, 'eslam', 'mahmoud', 'eslammahmoud@gmail.com', '987654', 2)
+INSERT INTO [dbo].[Student] ([ST_id], [ST_Fname], [ST_Lname], [ST_Email], [ST_Password], [Dept_Id]) VALUES (3, 'ahmed', 'osman', 'ahmedosman@gmail.com', '151515', 3)
