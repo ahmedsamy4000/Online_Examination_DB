7ad6d6accaa19648cc444478294269fac4fd150c
mokhtar <mokhtarmostafa1526@gmail.com>
2024-01-26 19:44:29 +0200

    Table 'Course' selection Stored Procedure with name 'Courses_Insertion' is added

---
+++ Stored Procedures/dbo.Course_Insertion.sql
@@ -0,0 +1,12 @@
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+CREATE proc [dbo].[Course_Insertion] @cid int, @cName varchar(50),@insID int
+as
+	if not exists(select Crs_ID from Course where @cid = Crs_ID)	 
+		insert into Course
+		values(@cid,@cName,@insID)
+	else
+		select 'Duplicate ID'
+GO
