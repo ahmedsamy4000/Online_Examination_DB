426ab32e0b0c46ccff1f68835f3b78cfc70235d4
19peter <peterashrafmail@gmail.com>
2024-01-27 13:44:02 +0200

    Add Insert data into Instructor table SP

--- Data/dbo.Instructor_Data.sql
+++ Data/dbo.Instructor_Data.sql
@@ -3,3 +3,4 @@ INSERT INTO [dbo].[Instructor] ([Ins_ID], [Ins_Name], [Dep_ID]) VALUES (2, 'Ahme
 INSERT INTO [dbo].[Instructor] ([Ins_ID], [Ins_Name], [Dep_ID]) VALUES (3, 'Yassmen Mohy', 3)
 INSERT INTO [dbo].[Instructor] ([Ins_ID], [Ins_Name], [Dep_ID]) VALUES (4, 'Nouran Sayed', 4)
 INSERT INTO [dbo].[Instructor] ([Ins_ID], [Ins_Name], [Dep_ID]) VALUES (5, 'Peter Ashraf', 5)
+INSERT INTO [dbo].[Instructor] ([Ins_ID], [Ins_Name], [Dep_ID]) VALUES (6, 'Phoebe Buffay', 6)
