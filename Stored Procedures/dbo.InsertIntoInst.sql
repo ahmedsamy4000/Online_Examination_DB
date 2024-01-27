ba7a5fbb390f6877d0e2639ed3bd4cf65c7ecf24
19peter <peterashrafmail@gmail.com>
2024-01-27 13:48:25 +0200

    Add saftey check into instructor insert SP

--- Stored Procedures/dbo.InsertIntoInst.sql
+++ Stored Procedures/dbo.InsertIntoInst.sql
@@ -4,8 +4,13 @@ SET ANSI_NULLS ON
 GO
 CREATE PROC [dbo].[InsertIntoInst] @id int, @name varchar(20), @DeptId int
 AS
+BEGIN try
 INSERT INTO dbo.Instructor
 (Ins_ID, Ins_Name, Dep_ID)
 VALUES
 ( @id, @name, @DeptId  )
+END TRY
+BEGIN CATCH
+SELECT 'invalid inputs'
+END CATCH
 GO
