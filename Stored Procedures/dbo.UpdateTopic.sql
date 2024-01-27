cddadde3c06f1d2482671f4f66c5f14be9886994
yasmeen-mohy <130379431+yasmeen-mohy@users.noreply.github.com>
2024-01-27 19:01:12 +0200

    Update Topic SP

---
+++ Stored Procedures/dbo.UpdateTopic.sql
@@ -0,0 +1,15 @@
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+CREATE PROCEDURE [dbo].[UpdateTopic]
+    @Topic_ID INT,
+    @Topic_Name VARCHAR(50),
+    @Crs_ID INT
+AS
+BEGIN
+    UPDATE Topic
+    SET Topic_Name = @Topic_Name, Crs_ID = @Crs_ID
+    WHERE Topic_ID = @Topic_ID
+END
+GO
