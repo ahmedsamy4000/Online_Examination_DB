255a3d6a0ebde9c255a24d06489e7662da210fda
19peter <peterashrafmail@gmail.com>
2024-02-07 16:48:59 +0200

    Adding Grade Col in Student_Course Table

--- Tables/dbo.Student_Course.sql
+++ Tables/dbo.Student_Course.sql
@@ -1,12 +1,13 @@
 CREATE TABLE [dbo].[Student_Course]
 (
 [St_ID] [int] NOT NULL,
-[Crs_ID] [int] NOT NULL
+[Crs_ID] [int] NOT NULL,
+[Final_Grade] [int] NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[Student_Course] ADD CONSTRAINT [PK_Student_Course] PRIMARY KEY CLUSTERED ([St_ID], [Crs_ID]) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[Student_Course] ADD CONSTRAINT [FK_Student_Course_Course] FOREIGN KEY ([Crs_ID]) REFERENCES [dbo].[Course] ([Crs_ID])
 GO
-ALTER TABLE [dbo].[Student_Course] ADD CONSTRAINT [FK_Student_Course_Student] FOREIGN KEY ([St_ID]) REFERENCES [dbo].[Student] ([ST_id])
+ALTER TABLE [dbo].[Student_Course] ADD CONSTRAINT [FK_Student_Course_Student] FOREIGN KEY ([St_ID]) REFERENCES [dbo].[Student] ([St_ID])
 GO
