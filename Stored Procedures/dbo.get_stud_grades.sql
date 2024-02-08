658a73e8ad2bc1fc23d78ab3c61d0ae5c7a00142
mokhtar <mokhtarmostafa1526@gmail.com>
2024-02-08 14:18:15 +0200

    edit in report number2

--- Stored Procedures/dbo.get_stud_grades.sql
+++ Stored Procedures/dbo.get_stud_grades.sql
@@ -6,10 +6,12 @@ CREATE proc [dbo].[get_stud_grades] @stud_id int
 as
 	if exists(select * from Student where @stud_id = St_ID)
 	begin
-		select Crs_name,Final_Grade
-		from Student_Course as sc inner join Course as c
-		on sc.Crs_ID = c.Crs_ID
-		where St_ID = @stud_id and Final_Grade is not null
+		select St_ID,Crs_name,Final_Grade
+		from Student_Course as sc ,Course as c 
+		where 
+		sc.Crs_ID = c.Crs_ID 
+		and St_ID = @stud_id
+		and Final_Grade is not null
 	end
 	else
 		select 'student id is not found'
