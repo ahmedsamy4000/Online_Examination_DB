43f24ebd18c77a8cbb2fd399bfcfc3910b9319ac
Nouran222 <nouransayednasr@gmail.com>
2024-02-09 13:28:13 +0200

    Exam_Insert updated.

--- Stored Procedures/dbo.Exam_Insert.sql
+++ Stored Procedures/dbo.Exam_Insert.sql
@@ -2,11 +2,11 @@ SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
 GO
-CREATE PROC [dbo].[Exam_Insert] @Ex_date DATE, @Ex_duration INT, @course_ID INT
+CREATE PROC [dbo].[Exam_Insert] @Ex_date DATE, @Ex_duration INT, @course_ID INT, @Ex_Grade INT
 AS 
 BEGIN TRY
-	INSERT INTO Exam(Date,Duration,Crs_ID)
-	VALUES(@Ex_date, @Ex_duration,@course_ID)
+	INSERT INTO Exam(Date,Duration,Crs_ID,Exam_Grade)
+	VALUES(@Ex_date, @Ex_duration,@course_ID,@Ex_Grade)
 END TRY 
 BEGIN CATCH
 	SELECT'Can not insert'
